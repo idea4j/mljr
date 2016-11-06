a79e030aa7bec8e8e791751450285aa6aa2d4330
zicheng.liang <zicheng.liang@mljr.com>
2016-11-06 22:00:12 +0800

    haha

---
+++ mljr/src/main/java/com/taotao/portal/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.taotao.portal.controller;
+
+/**
+ * @Description:
+ * @Author: 牛神成--Tinker
+ * @Company:
+ * @CreateDate: 16/11/4
+ */
+public class UserController {
+}
---
+++ mljr/src/test/java/com/mljr/demo/JedisDemo.java
@@ -0,0 +1,10 @@
+package com.mljr.demo;
+
+/**
+ * @Description:
+ * @Author: 牛神成--Tinker
+ * @Company:
+ * @CreateDate: 16/11/4
+ */
+public class JedisDemo {
+}
